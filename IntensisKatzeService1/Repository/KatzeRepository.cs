24866dc7021b9e2d93581d8bdf8d2819a18eacf0
Bojan Blagojevic <Bojan.Blagojevic@viziya.com>
2020-10-12 23:12:50 +0200

    katze service update

--- IntensisKatzeService1/Repository/KatzeRepository.cs
+++ IntensisKatzeService1/Repository/KatzeRepository.cs
@@ -21,114 +21,77 @@ namespace IntensisKatzeService1.Repository
         }
         public string GetMemoryIdByEmail(string email)
         {
-  
-            string query = "select * from Zaposleni where email='" + email + "'";
-            DataTable dt = GetData(query);
-            var idZaposlenog = (from DataRow dr in dt.Rows
-                                select (string)dr["IDZaposlenog"]).FirstOrDefault();
-            string query2 = "SELECT  IDNo FROM [Zaposleni_IDMemorija]" +
-                                 "WHERE DatumDodeljivanja = (SELECT max(DatumDodeljivanja) from Zaposleni_IDMemorija WHERE IDZaposlenog ="
-                                 + idZaposlenog + ") and IDZaposlenog =" + idZaposlenog;
-            DataTable dt2 = GetData(query2);
-            var idNo = (from DataRow dr in dt2.Rows
-                        select (string)dr["IDNo"]).FirstOrDefault();
-            return idNo;
+            string commandText1 = "SELECT IDZaposlenog FROM Zaposleni WHERE email = @email";
+
+
+            string commandText2 = "SELECT  substring(IDNo,7,8) FROM [Zaposleni_IDMemorija]"
+                                + " WHERE DatumDodeljivanja = (SELECT max(DatumDodeljivanja) from Zaposleni_IDMemorija WHERE IDZaposlenog = @IDZaposlenog)"
+                                + "  and IDZaposlenog = @IDZaposlenog  ";
+
+
+            using (SqlConnection conn = new SqlConnection(_configuration["ConnectionString"]))
+            {
+                string memoryID;
+                SqlCommand cmd = new SqlCommand(commandText1, conn);
+                conn.Open();
+                cmd.Parameters.AddWithValue("@email", email);
+                var katzeEmployeeID = cmd.ExecuteScalar();
+                if (katzeEmployeeID != null)
+                {
+                    cmd.CommandText = commandText2;
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@IDZaposlenog", katzeEmployeeID.ToString());
+                    var objmemID = cmd.ExecuteScalar();
+                    memoryID = objmemID == null ? throw new Exception($"Can't find memory id for {email}") : objmemID.ToString();
+                }
+                else
+                    throw new Exception($"Can't find katze id for {email} ");
+
+                return memoryID;
+            }
         }
 
         public int InsertRemoteWork(int minutes, string memory, string userName, DateTime? createdTime)
         {
 
-            string query = "insert into tblReg (KorisnickoIme, IDNo, TerminalskoVremeRegistracije, IDLogickeAktivnosti) values (@KorisnickoIme, substring(@IDNo,7,8), @TerminalskoVremeRegistracije, @IDLogickeAktivnosti);";
-            var parameters = new IDataParameter[]
-           {
-                new SqlParameter("@KorisnickoIme", userName.ToString()),
-                new SqlParameter("@IDNo", memory.ToString()),
-                new SqlParameter("@TerminalskoVremeRegistracije", createdTime.HasValue ? createdTime.Value.AddHours(-minutes/60) : (DateTime?)null),
-                new SqlParameter("@IDLogickeAktivnosti", 'U'),
-
-           };
-            ExecuteData(query, parameters);
-
-            string query2 = "insert into tblReg (KorisnickoIme, IDNo, TerminalskoVremeRegistracije, IDLogickeAktivnosti) values (@KorisnickoIme, substring(@IDNo,7,8), @TerminalskoVremeRegistracije, @IDLogickeAktivnosti);";
-            var parameters2 = new IDataParameter[]
-           {
-                new SqlParameter("@KorisnickoIme", userName.ToString()),
-                new SqlParameter("@IDNo", memory.ToString()),
-                new SqlParameter("@TerminalskoVremeRegistracije", createdTime ?? (DateTime?)null),
-                new SqlParameter("@IDLogickeAktivnosti", 'I'),
-
-           };
-            var cnt = ExecuteData(query2, parameters2);
-            return cnt;
-
-        }
-
-        public DataTable GetData(string str)
-        {
-            var logger = LogManager.GetLogger(typeof(KatzeRepository));
-            logger.Info("Getting data from katze");
-            DataTable objresutl = new DataTable();
-            try
-            {
-                SqlDataReader myReader;
-
-                using (SqlConnection myCon = new SqlConnection(_configuration["ConnectionString"]))
-                {
-                    myCon.Open();
-                    using (SqlCommand myCommand = new SqlCommand(str, myCon))
-                    {
-                        logger.Info("Connection is open");
-                        logger.Info("ExecuteReader");
-                        myReader = myCommand.ExecuteReader();
-                        objresutl.Load(myReader);
-
-                        myReader.Close();
-                        myCon.Close();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.Info(ex.Message);
-                throw ex;
-
-            }
-
-            logger.Info(objresutl.Rows);
-            return objresutl;
-
-        }
-        public int ExecuteData(string str, params IDataParameter[] sqlParams)
-        {
-            var logger = LogManager.GetLogger(typeof(KatzeRepository));
-            logger.Info("Insert in katze");
-
+            string commandTextU = "INSERT INTO [dbo].[tblReg] ([IDNo] ,[KorisnickoIme] ,[IDTerminala] ,[TerminalskoVremeRegistracije],"
+                                + "[IDLogickeAktivnosti],[Nevidljiva],[RegType]) "
+                                + " VALUES(@MemoryID, @UserName, -10,  DATEADD(minute,-@Minutes,@EntryDateTime)  ,"
+                                + " 'U', 0, 0) ";
+            string commandTextI = "INSERT INTO [dbo].[tblReg] ([IDNo] ,[KorisnickoIme] ,[IDTerminala] ,[TerminalskoVremeRegistracije],"
+                                + "[IDLogickeAktivnosti],[Nevidljiva],[RegType]) "
+                                + " VALUES(@MemoryID,  @UserName, -10, @EntryDateTime ,"
+                                + " 'I', 0, 0) ";
             using (SqlConnection conn = new SqlConnection(_configuration["ConnectionString"]))
             {
                 conn.Open();
-           
-                using (SqlCommand cmd = new SqlCommand(str, conn))
+                SqlTransaction transaction = conn.BeginTransaction("InsertKatze");
+                using (SqlCommand cmd = new SqlCommand(commandTextU, conn))
                 {
-                    if (sqlParams != null)
+                    cmd.Transaction = transaction;
+                    cmd.Parameters.AddWithValue("@MemoryID", memory);
+                    cmd.Parameters.Add("@EntryDateTime", System.Data.SqlDbType.DateTime);
+                    cmd.Parameters["@EntryDateTime"].Value = createdTime;
+                    cmd.Parameters.AddWithValue("@Minutes", minutes);
+                    cmd.Parameters.AddWithValue("@UserName", userName);
+                    try
                     {
-                        foreach (IDataParameter para in sqlParams)
-                        {
-                            cmd.Parameters.Add(para);
-                        }
-                        try
-                         {
-                            var cnt = cmd.ExecuteNonQuery();                       
-                          return cnt;
-                        } catch (Exception ex)
-                        {                        
-                            throw ex;
-                        }
+                        var cnt = cmd.ExecuteNonQuery();
+                        cmd.CommandText = commandTextI;
+                        cnt = cmd.ExecuteNonQuery();
+                        transaction.Commit();
+                        return cnt;
                     }
-                    return 0;
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+
                 }
             }
         }
-
+      
 
         public bool IsEntryInKatze(DateTime? dateOfActivity)
         {
